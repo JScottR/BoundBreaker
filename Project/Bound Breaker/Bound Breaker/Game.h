3601e4fc67485e672797767da1f4ea7baacb197d
Victor Manuel Palacios Rivera <vipa3492@librcomm-22.colorado.edu>
2015-03-31 17:53:23 -0600

    Left and Right Movement

--- Project/Bound Breaker/Bound Breaker/Game.h
+++ Project/Bound Breaker/Bound Breaker/Game.h
@@ -8,24 +8,47 @@
 
 #import <UIKit/UIKit.h>
 
+bool Right;
+bool Left;
+
 int RandomObstacleSingle1Placement;
 int RandomObstacleSingle2Placement;
 int RandomObstacleSingle3Placement;
 
+int ScoreNumber;
+
 @interface Game : UIViewController
 {
     IBOutlet UIImageView *Ball;
+    IBOutlet UIButton *TapRight;
+    IBOutlet UIButton *TapLeft;
     IBOutlet UIButton *StartGame;
     IBOutlet UIButton *GameOver;
+    
     IBOutlet UIImageView *ObstacleSingle1;
     IBOutlet UIImageView *ObstacleSingle2;
     IBOutlet UIImageView *ObstacleSingle3;
     
+    IBOutlet UILabel *WelcomeLabel;
+    IBOutlet UILabel *ScoreLabel;
+    
+    NSTimer *MoveRightTimer;
+    NSTimer *MoveLeftTimer;
     NSTimer *ObstacleMovement;
 }
 
 -(IBAction)StartGame:(id)sender;
+-(void)MoveRight;
+-(void)MoveLeft;
+-(IBAction)RightPress:(id)sender;
+-(IBAction)RightLiftInside:(id)sender;
+-(IBAction)RightLiftOutside:(id)sender;
+-(IBAction)LeftPress:(id)sender;
+-(IBAction)LeftLiftInside:(id)sender;
+-(IBAction)LeftLiftOutside:(id)sender;
 -(void)ObstaclesMoving;
 -(void)PlaceObstacles;
+-(void)GameOver;
+-(void)IncreaseScore;
 
 @end
--- Project/Bound Breaker/Bound Breaker/Game.m
+++ Project/Bound Breaker/Bound Breaker/Game.m
@@ -16,12 +16,60 @@
 
 -(IBAction)StartGame:(id)sender{
     StartGame.hidden = TRUE;
+    TapRight.hidden = FALSE;
+    TapLeft.hidden = FALSE;
+    Right = FALSE;
+    Left = FALSE;
     ObstacleSingle1.hidden = FALSE;
     ObstacleSingle2.hidden = FALSE;
     ObstacleSingle3.hidden = FALSE;
+    WelcomeLabel.hidden = TRUE;
     
+    MoveRightTimer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(MoveRight) userInfo:nil repeats:TRUE];
+     MoveLeftTimer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(MoveLeft) userInfo:nil repeats:TRUE];
+                      
     [self PlaceObstacles];
     ObstacleMovement = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(ObstaclesMoving) userInfo:nil repeats:TRUE];
+}
+
+
+
+-(IBAction)RightPress:(id)sender{
+    Right = TRUE;
+}
+-(IBAction)RightLiftInside:(id)sender{
+    Right = FALSE;
+}
+-(IBAction)RightLiftOutside:(id)sender{
+    Right = FALSE;
+}
+
+-(IBAction)LeftPress:(id)sender{
+    Left = TRUE;
+}
+-(IBAction)LeftLiftInside:(id)sender{
+    Left = FALSE;
+}
+-(IBAction)LeftLiftOutside:(id)sender{
+    Left = FALSE;
+}
+
+-(void)MoveRight{
+    if(Right == TRUE){
+        Ball.center = CGPointMake(Ball.center.x + 1, Ball.center.y);
+    }
+}
+
+-(void)MoveLeft{
+    if(Left == TRUE){
+        Ball.center = CGPointMake(Ball.center.x - 1, Ball.center.y);
+    }
+}
+
+
+-(void)IncreaseScore{
+    ScoreNumber = ScoreNumber + 1;
+    ScoreLabel.text = [NSString stringWithFormat:@"%i", ScoreNumber];
 }
 
 -(void)ObstaclesMoving{
@@ -42,6 +90,10 @@
     if(CGRectIntersectsRect(Ball.frame, ObstacleSingle3.frame)){
         Ball.center = CGPointMake(Ball.center.x, Ball.center.y + 1);
     }
+    
+    if(Ball.center.y > 475) {
+        [self GameOver];
+    }
 }
 
 -(void)PlaceObstacles{
@@ -55,15 +107,30 @@
     ObstacleSingle3.center = CGPointMake(RandomObstacleSingle3Placement, 0);
 }
 
-- (void)viewDidLoad {
-    GameOver.hidden = TRUE;
-    StartGame.hidden = FALSE;
+-(void)GameOver{
+    GameOver.hidden = FALSE;
+    Ball.hidden = TRUE;
     ObstacleSingle1.hidden = TRUE;
     ObstacleSingle2.hidden = TRUE;
     ObstacleSingle3.hidden = TRUE;
+}
+
+- (void)viewDidLoad {
+    GameOver.hidden = TRUE;
+    StartGame.hidden = FALSE;
+    TapRight.hidden = TRUE;
+    TapLeft.hidden = TRUE;
+    Right = FALSE;
+    Left = FALSE;
+    ObstacleSingle1.hidden = TRUE;
+    ObstacleSingle2.hidden = TRUE;
+    ObstacleSingle3.hidden = TRUE;
+    WelcomeLabel.hidden = FALSE;
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
