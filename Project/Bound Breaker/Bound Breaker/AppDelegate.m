9ef9c836fba54861fa04f4dff2ef8ef3c2eb8709
JScott <john.s.richards@colorado.edu>
2015-04-01 22:13:54 -0600

    Added Parse Framework and Login option
    Users can now login into their accounts and register new accounts.
    Parse also adds a lot of features we can implement later into the app

--- Project/Bound Breaker/Bound Breaker/AppDelegate.m
+++ Project/Bound Breaker/Bound Breaker/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Parse setApplicationId:@"0T0sGEPIFN4KMEakMLMeGt8xJxrw13WduEkbTLSv"
+                  clientKey:@"wkTuZN5iN3WE78SznP5gRRfEY73dHBMpIFUXytM8"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
+    
     return YES;
 }
 
---
+++ Project/Bound Breaker/Bound Breaker/ProfileViewController.m
@@ -0,0 +1,41 @@
+//
+//  ProfileViewController.m
+//  Bound Breaker
+//
+//  Created by JScott Richards on 4/1/15.
+//
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)logoutButton:(id)sender {
+    [PFUser logOut];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
